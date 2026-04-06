0d004c6412180093cf9c4953ba4fc7f9ddafddfa
paulonova <paulonova144@gmail.com>
2026-04-06 14:33:12 +0200

    Wordpress Script install and run

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '7661139b46942fd8d70d');
--- functions.php
+++ functions.php
@@ -28,7 +28,7 @@ function knowledge_files() {
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
   wp_enqueue_style('parisienne-font', '//fonts.googleapis.com/css2?family=Parisienne&display=swap');
   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-  wp_enqueue_style('knowledge_main_styles', get_theme_file_uri('/build/style.css'));
+  wp_enqueue_style('knowledge_main_styles', get_theme_file_uri('/build/style-index.css'));
 }
 
 // Hook the knowledge_files function to the wp_enqueue_scripts action to load scripts and styles properly.
